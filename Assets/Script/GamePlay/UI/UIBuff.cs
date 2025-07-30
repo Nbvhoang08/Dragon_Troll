662fc9458f7dd6cb412a74a511a8a0afdb513ccc
Nbvhoang08 <hoangnbv@hapiga.net>
2025-07-30 17:51:49 +0700

    UI

---
+++ Assets/Script/GamePlay/UI/UIBuff.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIBuff : UICanvas{
+    // Start is called before the first frame update
+    private void OnEnable()
+    {
+        
+    }
+    private void OnDisable()
+    {
+
+    }
+    void AnimationFade() 
+    {
+        
+    
+    }
+}
---
+++ Assets/Script/GamePlay/UI/UIHome.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.PlasticSCM.Editor.WebApi;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Analytics;
+
+public class UIHome : UICanvas
+{
+    public void PlayBtn() 
+    {
+      
+    }
+    private void UpdateLevelText()
+    {
+        
+    }
+
+    void SomeThing() 
+    {
+        
+    }
+}
